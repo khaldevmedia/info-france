42abfbbe7ca31e30c6759eede8f2d4441d26f970
Khaled <kal@someemailadress.com>
2024-04-04 00:53:46 +0200

    Use Grid fo Autocomplete and Chip Fix counter in Coummunes.jsx

--- src/componants/routes/Communes.jsx
+++ src/componants/routes/Communes.jsx
@@ -3,7 +3,14 @@ import api from "../../api/geoAPI";
 import { AppContext } from "../main/AppContext";
 import CommuneDetails from "../SubComponants/CommuneDetails";
 
-import { Box, Typography, TextField, Autocomplete, Chip } from "@mui/material";
+import {
+  Box,
+  Typography,
+  TextField,
+  Autocomplete,
+  Chip,
+  Grid,
+} from "@mui/material";
 
 function Communes() {
   const [communes, setCommunes] = useState([]);
@@ -55,73 +62,80 @@ function Communes() {
         <Box
           sx={{
             width: "100%",
+            maxWidth: { md: 600 },
             mb: 3,
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
           }}
         >
-          <Autocomplete
-            id="commune-combo-box"
-            sx={{ width: { xs: "80%", md: "100%" }, maxWidth: { md: 500 } }}
-            options={communes}
-            getOptionLabel={(option) => option.nom}
-            getOptionKey={(option) => option.siren}
-            isOptionEqualToValue={(option, value) =>
-              option.siren === value.siren
-            }
-            renderOption={(props, option) => (
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "100%",
-                  borderBottom: 1,
-                  borderColor: "grey.300",
+          <Grid container spacing={{ md: 2, xs: 0 }}>
+            <Grid item xs={9}>
+              <Autocomplete
+                id="commune-combo-box"
+                sx={{ width: "100%" }}
+                options={communes}
+                getOptionLabel={(option) => option.nom}
+                getOptionKey={(option) => option.siren}
+                isOptionEqualToValue={(option, value) =>
+                  option.siren === value.siren
+                }
+                renderOption={(props, option) => (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      width: "100%",
+                      borderBottom: 1,
+                      borderColor: "grey.300",
+                    }}
+                    component="li"
+                    {...props}
+                  >
+                    <Box sx={{ width: "60%" }}>
+                      <b>{option.nom}</b>
+                    </Box>
+                    <Box sx={{ width: "40%" }}>
+                      {option.departement.code} - {option.departement.nom}
+                    </Box>
+                  </Box>
+                )}
+                onInputChange={(event, newInputValue) => {
+                  setInputValue(newInputValue.trim());
                 }}
-                component="li"
-                {...props}
-              >
-                <Box sx={{ width: "60%" }}>
-                  <b>{option.nom}</b>
-                </Box>
-                <Box sx={{ width: "40%" }}>
-                  {option.departement.code} - {option.departement.nom}
-                </Box>
-              </Box>
-            )}
-            onInputChange={(event, newInputValue) => {
-              setInputValue(newInputValue.trim());
-            }}
-            onChange={(event, newValue) => {
-              setSelectedCommune(newValue);
-              if (newValue) {
-                updateCenter(
-                  newValue.centre.coordinates[1],
-                  newValue.centre.coordinates[0]
-                );
-              }
-            }}
-            noOptionsText={"Acune commune trouvée"}
-            clearText={"Effacer"}
-            openText={"Ouvrir"}
-            blurOnSelect={"touch"}
-            renderInput={(params) => (
-              <TextField {...params} label="Chercher des communes" />
-            )}
-          />
-          <Chip
-            label={communes.length}
-            color={"primary"}
-            sx={{
-              fontSize: { md: 20, xs: 16 },
-              fontWeight: 400,
-              ml: 1,
-              height: 50,
-              minWidth: 50,
-              width: "auto",
-            }}
-          />
+                onChange={(event, newValue) => {
+                  setSelectedCommune(newValue);
+                  if (newValue) {
+                    updateCenter(
+                      newValue.centre.coordinates[1],
+                      newValue.centre.coordinates[0]
+                    );
+                  }
+                }}
+                noOptionsText={"Acune commune trouvée"}
+                clearText={"Effacer"}
+                openText={"Ouvrir"}
+                blurOnSelect={"touch"}
+                renderInput={(params) => (
+                  <TextField {...params} label="Chercher des communes" />
+                )}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Chip
+                label={communes.length}
+                color={"primary"}
+                sx={{
+                  fontSize: { md: 20, xs: 16 },
+                  fontWeight: 400,
+                  ml: 1,
+                  height: 50,
+                  minWidth: 50,
+                  width: "auto",
+                }}
+              />
+            </Grid>
+          </Grid>
         </Box>
       </Box>
       <Box
