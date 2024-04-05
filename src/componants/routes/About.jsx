921bff97ee34709eed5b630d02d164d0e3132dfb
Khaled <kal@someemailadress.com>
2024-04-05 23:41:46 +0200

    Update fill attribute of svg in  About.jsx

--- src/componants/routes/About.jsx
+++ src/componants/routes/About.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Box, Typography, Link, Alert } from "@mui/material";
 
+import { useTheme } from "@mui/material/styles";
+
 function About() {
+  const theme = useTheme();
   return (
     <Box component="main" sx={{ height: "100%", px: { md: 3, xs: 2 } }}>
       <Typography sx={{ mb: 2 }} variant="h4">
@@ -21,7 +24,7 @@ function About() {
             data-testid="ArrowOutwardRoundedIcon"
             height="13"
             width="13"
-            fill="#1976d2"
+            fill={theme.palette.primary.main}
           >
             <path d="M6 7c0 .55.45 1 1 1h7.59l-8.88 8.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L16 9.41V17c0 .55.45 1 1 1s1-.45 1-1V7c0-.55-.45-1-1-1H7c-.55 0-1 .45-1 1"></path>
           </svg>
